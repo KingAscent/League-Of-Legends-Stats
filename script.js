506b8ae456faf9e67b8f19848c943bf6571dbf11
KingAscent <86906764+KingAscent@users.noreply.github.com>
2023-02-05 09:06:44 -0800

    We are now able to access and retrieve champion mastery, level, and points

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 /*
-RGAPI-430b27d2-ed43-49e8-bd64-56c2600bae20
-const rgkey = 'api_key = RGAPI-430b27d2-ed43-49e8-bd64-56c2600bae20';
-const personalKey = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Vinmai?api_key=RGAPI-430b27d2-ed43-49e8-bd64-56c2600bae20'
+RGAPI-107bc540-5139-42df-b3ab-c8130df07fc4
+const rgkey = 'api_key = RGAPI-107bc540-5139-42df-b3ab-c8130df07fc4';
+const personalKey = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Vinmai?api_key=RGAPI-107bc540-5139-42df-b3ab-c8130df07fc4'
 const space = '$20';
 */
 
-async function fetchSumByName(name, type){
+async function fetchSumByName(name){
     // Get summoner information from Riot Games API
-    let url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=RGAPI-430b27d2-ed43-49e8-bd64-56c2600bae20";
+    let url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=RGAPI-107bc540-5139-42df-b3ab-c8130df07fc4";
     let res = await fetch(url)
-
     // Turn the response into a json and return it
     let data = await res.json();
-    
+    return data;
+}
+
+async function fetchChampMastery(id){
+    let url = "https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + id + "?api_key=RGAPI-107bc540-5139-42df-b3ab-c8130df07fc4";
+    let res = await fetch(url);
+    // Turn the response into a json and return it
+    let data = await res.json();
     return data;
 }
 
 async function main(){
-    let data = await fetchSumByName("Kanan Matsuura");
+    // Prompt the user for the summoner name
+    let name = "Kanan Matsuura";
+    // Get the summoner's account information needed for all following methods
+    let data = await fetchSumByName(name);
     console.log(data);
     // Retrieve champion mastery for this summoner
-
+    let champMastery = await fetchChampMastery(data.id);
+    // Create an array to contain all champion info
+    champInfo = [];
+    champMastery.forEach(champ => {
+        // Store champ information in the format:
+        // champID | champLevel | champPoints
+        champInfo.push([champ.championId, champ.championLevel, champ.championPoints]);
+    });
+    
     console.log("We are here");
 }
 
