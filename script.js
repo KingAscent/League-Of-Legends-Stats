f5acc6820a2e0b9f86cb95ab3488d1b8dee1f53f
KingAscent <86906764+KingAscent@users.noreply.github.com>
2023-02-06 16:56:41 -0800

    Can construct a table of a summoner's champ data

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-/*
-RGAPI-107bc540-5139-42df-b3ab-c8130df07fc4
-const rgkey = 'api_key = RGAPI-107bc540-5139-42df-b3ab-c8130df07fc4';
-const personalKey = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Vinmai?api_key=RGAPI-107bc540-5139-42df-b3ab-c8130df07fc4'
-const space = '$20';
-*/
+let key = "RGAPI-00004247-ec7e-4803-919d-433095438f67";
 
 async function fetchSumByName(name){
     // Get summoner information from Riot Games API
-    let url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=RGAPI-107bc540-5139-42df-b3ab-c8130df07fc4";
+    let url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + key;
     let res = await fetch(url)
     // Turn the response into a json and return it
     let data = await res.json();
@@ -15,14 +10,44 @@ async function fetchSumByName(name){
 }
 
 async function fetchChampMastery(id){
-    let url = "https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + id + "?api_key=RGAPI-107bc540-5139-42df-b3ab-c8130df07fc4";
+    let url = "https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + id + "?api_key=" + key;
     let res = await fetch(url);
     // Turn the response into a json and return it
     let data = await res.json();
     return data;
 }
 
+function infoTable(info){
+    let headers = ["Champion", "Mastery Level", "Mastery Points"];
+    let table = document.createElement("Table");
+    for(let i = 0; i < info.length; i++){
+        let data = table.insertRow(i);
+        data.insertCell(0).innerHTML = info[i][0];
+        data.insertCell(1).innerHTML = info[i][1];
+        data.insertCell(2).innerHTML = info[i][2];
+    }
+    let header = table.createTHead();
+    row = header.insertRow();
+    for(let i = 0; i < headers.length; i++){
+        row.insertCell(i).innerHTML = headers[i];
+    }
+    document.body.append(table);
+}
+
 async function main(){
+/*
+    // Get champions in the format: name | ID
+    let championData = await fetch("http://ddragon.leagueoflegends.com/cdn/13.1.1/data/en_US/champion.json");
+    let champData = await championData.json();
+    let champID = Array(champData.length).fill("empty");
+    // Retrieve each champion's name through their championID, and replace champID with it
+    for(let i = 0; i < champData.length; i++){
+        let champ = champData.data;
+        
+    }
+    console.log(champData.data);
+*/
+
     // Prompt the user for the summoner name
     let name = "Kanan Matsuura";
     // Get the summoner's account information needed for all following methods
@@ -37,7 +62,8 @@ async function main(){
         // champID | champLevel | champPoints
         champInfo.push([champ.championId, champ.championLevel, champ.championPoints]);
     });
-    
+    // Create a table of information from the champ Mastery information
+    infoTable(champInfo);
     console.log("We are here");
 }
 
