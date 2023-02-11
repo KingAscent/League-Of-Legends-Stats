6a015f97df298400f5c6df8be5ff96f477e86ba5
KingAscent <86906764+KingAscent@users.noreply.github.com>
2023-02-10 19:05:35 -0800

    Added page text describing the purpose.

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-//TODO: Line 37: Incorportate total champ mastery value into table
 let key = "RGAPI-071df365-6f5a-42fa-a788-0dbc410886b2";
 
 async function fetchSumByName(){
     // Get summoner information from Riot Games API
     let data;
-    let name = prompt("Please input a summoner name.\nExample: Sayo");
+    let name = prompt("Please enter a summoner name.\nExample: Sayo");
     // Trap the user if they do not input a valid summoner name
     let found = false;
-    while(!found){
+    while(!found || name.toLowerCase() == "null"){
         let url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + key;
         try{
             let res = await fetch(url);
             found = true;
             data = res.json();
         }catch{
-            name = prompt("Name not found?");
+            name = prompt("Summoner name not found. Please enter a valid summoner name.\nExample: Sayo");
         }
     }
     return data;
@@ -29,16 +28,17 @@ async function fetchChampMastery(id){
 }
 
 function infoTable(info, totalMastery){
-    let headers = ["Champion", "Mastery Level", "Mastery Points", "Chest Obtained", "Last Date Played"];
+    let headers = ["Champion", "Mastery Level", "Mastery Points", "Chest Obtained", "Last Date Played", "Mastery Progress"];
     let table = document.createElement("table");
     table.style.textAlign = 'center';
     for(let i = 0; i < info.length; i++){
         let data = table.insertRow(i);
-        data.insertCell(0).innerHTML = info[i][0];
-        data.insertCell(1).innerHTML = info[i][1];
-        data.insertCell(2).innerHTML = info[i][2];
-        data.insertCell(3).innerHTML = info[i][3];
-        data.insertCell(4).innerHTML = info[i][4];
+        data.insertCell(0).innerHTML = info[i][0]; // Champion
+        data.insertCell(1).innerHTML = info[i][1]; // Mastery Level
+        data.insertCell(2).innerHTML = info[i][2]; // Mastery Points
+        data.insertCell(3).innerHTML = info[i][3]; // Chest Obtained
+        data.insertCell(4).innerHTML = info[i][4]; // Last Date Played
+        data.insertCell(5).innerHTML = info[i][5]; // Mastery Progress
         if(i % 2 != 0)
         data.style.backgroundColor = "#CCCDCD";
     }
@@ -70,12 +70,26 @@ function formatedChampionMastery(champMastery, findByKey){
         champLevel = champ.championLevel;
         champPoints = champ.championPoints.toLocaleString("en-US");
         chestObtained = champ.chestGranted;
+        if(chestObtained){
+            chestObtained = "Earned";
+        }else{
+            chestObtained = "Not Earned";
+        }
         lastPlayed = new Date(champ.lastPlayTime).toString().substring(4, 15);
         // Format the lastPlayed substring to an even more readable string
         lastPlayed = lastPlayed.substring(4, 7) + lastPlayed.substring(0, 4) + lastPlayed.substring(6, 11);
+        champProgress = champ.tokensEarned;
+        if(champLevel == 7)
+            champProgress = "Mastered";
+        else if(champLevel == 6)
+            champProgress = "Tokens earned: " + champProgress + " / 3";
+        else if(champLevel == 5)
+            champProgress = "Tokens earned: " + champProgress + " / 2";
+        else
+            champProgress = "Points until next level: " + champ.championPointsUntilNextLevel;
         // Store champ information in the format:
-        // champName | champLevel | champPoints | Chest Obtained | Last Time Champ Was Played
-        champInfo.push([champName, champLevel, champPoints, chestObtained, lastPlayed]);
+        // champName | champLevel | champPoints | Chest Obtained | Last Time Champ Was Played | Mastery Progress
+        champInfo.push([champName, champLevel, champPoints, chestObtained, lastPlayed, champProgress]);
     });
     return champInfo;
 }
@@ -87,8 +101,6 @@ async function main(){
     let champData = await championData.json();
     let findByKey = (matchKey) => Object.entries(champData.data).find(([key, value]) => value.key == matchKey);
 
-    // Prompt the user for the summoner name
-    //let name = "Kanan Matsuura";
     // Get the summoner's account information needed for all following methods
     let data = await fetchSumByName();
     // Retrieve champion mastery for this summoner
@@ -99,8 +111,8 @@ async function main(){
     champInfo = formatedChampionMastery(champMastery, findByKey);
     // Create a table of information from the champ Mastery information
     infoTable(champInfo, totalMastery);
-    console.log("We are here");
     console.log(data);
+    console.log("We've reached the end");
 }
 
 main();
